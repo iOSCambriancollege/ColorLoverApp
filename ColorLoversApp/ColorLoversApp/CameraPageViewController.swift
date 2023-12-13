548185f3baaf5b666a9fa429b78645cd846dd974
Hemal31 <133526950+Hemal31@users.noreply.github.com>
2023-12-13 00:55:56 -0500

    created image upload/camera page
    Added the elements to the page, connected a new UIViewController, and actions

---
+++ ColorLoversApp/ColorLoversApp/CameraPageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CameraPageViewController.swift
+//  ColorLoversApp
+//
+//  Created by Shaik Mathar Syed on 13/12/23.
+//
+
+import UIKit
+
+class CameraPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func openCameraButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func selectImageButtonTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
